172a87d3bd9bb83001da821842e9cc52765774a2
Vishnu Basskar V <60216237+Vish2476@users.noreply.github.com>
2020-04-29 18:31:08 +0530

    Week-5 Solution
    Complete solution Is Present for reference purposes only

---
+++ Week-5/index.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Vishnu Basskar V PHP</title>
+</head>
+<body>
+<h1>Vishnu Basskar V PHP</h1>
+<p>The ASCII ART of Letter V</p>
+<pre>ASCII ART:
+
+   **           **
+    **         **
+     **       **
+      **     **
+       **   **
+        ** **
+         ***
+</pre>
+<p>The SHA256 hash of "Vishnu Basskar V" is <?php print hash('sha256', 'Vishnu Basskar V'); ?> </p>
+<a href="check.php">Click here to check the error setting</a>
+<br/>
+<a href="fail.php">Click here to cause a traceback</a>
+</body>
